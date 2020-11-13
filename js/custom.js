90cd8d0c1b410df737855147deedb3519de589d0
Roberto Markus <markusrobertoo@gmail.com>
2020-11-13 21:08:31 +0700

    updated new files

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,26 @@
 setInterval(function() {
     //location.reload();
-}, 2000);
+}, 1000);
+
+// Slider
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value; 
+  this.style.background = 'linear-gradient(to right, #7b88ad 0%, #82CFD0 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+}
+
+// End of Slider
+
+//Status Indikator
+function toogle() {
+    var x = document.getElementById("bulbstatus");
+    if (x.innerHTML === "ON") {
+      x.innerHTML = "OFF";
+    } else {
+      x.innerHTML = "ON";
+    }
+  }
+//End of Status Indikator
